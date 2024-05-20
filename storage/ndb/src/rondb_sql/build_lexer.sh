d2615d77b78b157d1069ecb3e9fd70c0a4ffe583
Axel Svensson <axel@hopsworks.ai>
2024-05-20 14:00:12 +0200

    Fix #line directives in generated parser and lexer

--- storage/ndb/src/rondb_sql/build_lexer.sh
+++ storage/ndb/src/rondb_sql/build_lexer.sh
@@ -12,9 +12,13 @@ TARGET_RONDBSQLLEXER_L_CPP="$4"
 TMPDIR="$(mktemp -d)"
 trap 'rm -rf "$TMPDIR"' EXIT
 
+# As we are not quite satisfied with the flex output, we let flex output to
+# temporary files, allowing us to edit them before they are saved to the target
+# location.
+
 "$FLEX" \
     --header-file="$TMPDIR/RonDBSQLLexer.l.hpp" \
-    --outfile="$TMPDIR/RonDBSQLLexer.l.with-hold_char.cpp" \
+    --outfile="$TMPDIR/RonDBSQLLexer.l.cpp" \
     "$SOURCE_RONDBSQLLEXER_L" \
     2>&1 | \
     tee "$TMPDIR/RonDBSQLLexer.l.err"
@@ -45,24 +49,74 @@ if grep -qE "yy_hold_char| = '.0';" "$SOURCE_RONDBSQLLEXER_L"; then
     exit 1
 fi
 
-sed -r "/yy_hold_char/d; / = '.0';/d;" "$TMPDIR/RonDBSQLLexer.l.with-hold_char.cpp" > "$TMPDIR/RonDBSQLLexer.l.cpp"
+sed -r "/yy_hold_char/d; / = '.0';/d;" "$TMPDIR/RonDBSQLLexer.l.cpp" > "$TMPDIR/RonDBSQLLexer.l.fix_1.cpp"
 
-if diff -q "$TMPDIR/RonDBSQLLexer.l.with-hold_char.cpp" "$TMPDIR/RonDBSQLLexer.l.cpp"; then
+if diff -q "$TMPDIR/RonDBSQLLexer.l.cpp" "$TMPDIR/RonDBSQLLexer.l.fix_1.cpp"; then
     echo "Editing to remove hold_char ineffective."
     exit 1
 else
     echo "Confirmed that attempt to remove hold_char made some change."
 fi
 
-if grep -qE "yy_hold_char| = '.0';" "$TMPDIR/RonDBSQLLexer.l.cpp"; then
+if grep -qE "yy_hold_char| = '.0';" "$TMPDIR/RonDBSQLLexer.l.fix_1.cpp"; then
     echo "Editing to remove hold_char ineffective."
-    grep -HEn "yy_hold_char| = '.0';" "$TMPDIR/RonDBSQLLexer.l.cpp"
+    grep -HEn "yy_hold_char| = '.0';" "$TMPDIR/RonDBSQLLexer.l.fix_1.cpp"
     exit 1
 fi
 
+# Flex uses #line directives in the output file to aid error messages. Some of
+# them refer to the source file $SOURCE_RONDBSQLLEXER_L and others to the output
+# file. The latter are incorrect in two ways: We have removed some lines, making
+# the line number incorrect, and we will move the file to the target below,
+# making the file name incorrect. We use `awk` to fix those #line directives.
+
+AWK_SCRIPT='
+  # Handle #line directives specially
+  /^#line/ {
+    if ($3 == "\"" change_from_file "\"")
+    {
+      correct_line = NR + 1
+      # Correct both file file name and line number.
+      print "#line " correct_line " \"" change_to_file "\""
+      next
+    }
+    else if ($3 == "\"" no_touch_file "\"")
+    {
+      # This file name is expected, and should remain. The line number can be
+      # anything since it refers to another file, and should also remain.
+      print
+      next
+    }
+    else
+    {
+      # Unexpected file name
+      print "Expected file name " change_from_file " or " no_touch_file " but got: " $0 | "cat >&2"
+      exit 1
+    }
+  }
+  # Default action, applied to lines other than #line directives is to change
+  # nothing
+  { print }
+'
+
+awk \
+    -v change_from_file="$TMPDIR/RonDBSQLLexer.l.hpp" \
+    -v change_to_file="$TARGET_RONDBSQLLEXER_L_HPP" \
+    -v no_touch_file="$SOURCE_RONDBSQLLEXER_L" \
+    "$AWK_SCRIPT" \
+    "$TMPDIR/RonDBSQLLexer.l.hpp" \
+    > "$TMPDIR/RonDBSQLLexer.l.fix_1.hpp"
+awk \
+    -v change_from_file="$TMPDIR/RonDBSQLLexer.l.cpp" \
+    -v change_to_file="$TARGET_RONDBSQLLEXER_L_CPP" \
+    -v no_touch_file="$SOURCE_RONDBSQLLEXER_L" \
+    "$AWK_SCRIPT" \
+    "$TMPDIR/RonDBSQLLexer.l.fix_1.cpp" \
+    > "$TMPDIR/RonDBSQLLexer.l.fix_2.cpp"
+
 # Success
 
-mv "$TMPDIR/RonDBSQLLexer.l.hpp" "$TARGET_RONDBSQLLEXER_L_HPP"
-mv "$TMPDIR/RonDBSQLLexer.l.cpp" "$TARGET_RONDBSQLLEXER_L_CPP"
+mv "$TMPDIR/RonDBSQLLexer.l.fix_1.hpp" "$TARGET_RONDBSQLLEXER_L_HPP"
+mv "$TMPDIR/RonDBSQLLexer.l.fix_2.cpp" "$TARGET_RONDBSQLLEXER_L_CPP"
 
 echo "Done building RonDB SQL lexer."
--- storage/ndb/src/rondb_sql/build_parser.sh
+++ storage/ndb/src/rondb_sql/build_parser.sh
@@ -12,38 +12,93 @@ TARGET_RONDBSQLPARSER_Y_CPP="$4"
 TMPDIR="$(mktemp -d)"
 trap 'rm -rf "$TMPDIR"' EXIT
 
+# As we are not quite satisfied with the bison output, we let bison output to
+# temporary files, allowing us to edit them before they are saved to the target
+# location.
+
 "$BISON" \
     -Wall \
     -Wcounterexamples \
     -Wdangling-alias \
     -Werror \
     --header="$TMPDIR/RonDBSQLParser.y.hpp" \
-    --output="$TMPDIR/RonDBSQLParser.y.raw.cpp" \
+    --output="$TMPDIR/RonDBSQLParser.y.cpp" \
     "$SOURCE_RONDBSQLPARSER_Y"
 
 # We use `%define api.location.type` to declare a custom location type. However,
 # bison does not have any option for the default value of that type. So,
 # unfortunately we have to edit the generated file the hacky way.
 
-if [ "$(grep -Ec '  = \{ 1, 1, 1, 1 \}' "$TMPDIR/RonDBSQLParser.y.raw.cpp")" = 1 ]; then
-    echo "Confirmed that generated RonDBSQLParser.y.raw.cpp has exactly 1 occurrence of default location value."
+if [ "$(grep -Ec '  = \{ 1, 1, 1, 1 \}' "$TMPDIR/RonDBSQLParser.y.cpp")" = 1 ]; then
+    echo "Confirmed that generated RonDBSQLParser.y.cpp has exactly 1 occurrence of default location value."
 else
-    echo "Could not confirm that generated RonDBSQLParser.y.raw.cpp has exactly 1 occurrence of default location value."
+    echo "Could not confirm that generated RonDBSQLParser.y.cpp has exactly 1 occurrence of default location value."
     exit 1
 fi
 
-sed -r "s/  = \{ 1, 1, 1, 1 \}/  = { NULL, 0 }/" "$TMPDIR/RonDBSQLParser.y.raw.cpp" > "$TMPDIR/RonDBSQLParser.y.cpp"
+sed -r "s/  = \{ 1, 1, 1, 1 \}/  = { NULL, 0 }/" "$TMPDIR/RonDBSQLParser.y.cpp" > "$TMPDIR/RonDBSQLParser.y.fix_1.cpp"
 
-if diff -q "$TMPDIR/RonDBSQLParser.y.raw.cpp" "$TMPDIR/RonDBSQLParser.y.cpp"; then
+if diff -q "$TMPDIR/RonDBSQLParser.y.cpp" "$TMPDIR/RonDBSQLParser.y.fix_1.cpp"; then
     echo "Editing of default location value ineffective."
     exit 1
 else
     echo "Confirmed that editing of default location made some change."
 fi
 
+# bison uses #line directives in the output file to aid error messages. Some of
+# them refer to the source file $SOURCE_RONDBSQLPARSER_Y and others to the
+# output file. Since we let bison output to temporary files, the latter would
+# become incorrect when we move the file to the target below. We use `awk` to
+# fix these #line directives.
+
+AWK_SCRIPT='
+  # Handle #line directives specially
+  /^#line/ {
+    if ($3 == "\"" change_from_file "\"") {
+      correct_line = NR + 1
+      # Since we have not added or removed any lines, we expect the line number to already be correct.
+      if ($2 != correct_line) {
+        print "Expected line number " correct_line " but got: " $0 | "cat >&2"
+        exit 1
+      }
+      # This is the file name we are supposed to change.
+      print "#line " correct_line " \"" change_to_file "\""
+      next
+    }
+    else if ($3 == "\"" no_touch_file "\"") {
+      # This file name is expected, and should remain. The line number can be
+      # anything since it refers to another file.
+      print
+      next
+    } else {
+      # Unexpected file name
+      print "Expected file name " change_from_file " or " no_touch_file " but got: " $0 | "cat >&2"
+      exit 1
+    }
+  }
+  # Default action, applied to lines other than #line directives is to change
+  # nothing
+  { print }
+'
+
+awk \
+    -v change_from_file="$TMPDIR/RonDBSQLParser.y.hpp" \
+    -v change_to_file="$TARGET_RONDBSQLPARSER_Y_HPP" \
+    -v no_touch_file="$SOURCE_RONDBSQLPARSER_Y" \
+    "$AWK_SCRIPT" \
+    "$TMPDIR/RonDBSQLParser.y.hpp" \
+    > "$TMPDIR/RonDBSQLParser.y.fix_1.hpp"
+awk \
+    -v change_from_file="$TMPDIR/RonDBSQLParser.y.cpp" \
+    -v change_to_file="$TARGET_RONDBSQLPARSER_Y_CPP" \
+    -v no_touch_file="$SOURCE_RONDBSQLPARSER_Y" \
+    "$AWK_SCRIPT" \
+    "$TMPDIR/RonDBSQLParser.y.fix_1.cpp" \
+    > "$TMPDIR/RonDBSQLParser.y.fix_2.cpp"
+
 # Success
 
-mv "$TMPDIR/RonDBSQLParser.y.hpp" "$TARGET_RONDBSQLPARSER_Y_HPP"
-mv "$TMPDIR/RonDBSQLParser.y.cpp" "$TARGET_RONDBSQLPARSER_Y_CPP"
+mv "$TMPDIR/RonDBSQLParser.y.fix_1.hpp" "$TARGET_RONDBSQLPARSER_Y_HPP"
+mv "$TMPDIR/RonDBSQLParser.y.fix_2.cpp" "$TARGET_RONDBSQLPARSER_Y_CPP"
 
 echo "Done building RonDB SQL parser."
