0de78916605b88245a455d392a090e503c0896ed
Axel Svensson <axel@hopsworks.ai>
2025-03-19 10:44:52 +0100

    refactor

--- storage/ndb/include/util/unescapedAscii.cpp
+++ storage/ndb/include/util/unescapedAscii.cpp
@@ -397,6 +397,8 @@ __attribute__((always_inline)) static inline
 __attribute__((__target__ ("sse2,avx2")))
 bool unescaped_ascii_avx2(const char *str, const char *end) {
   unsigned int len = end - str;
+  const __m256i *const mstr = reinterpret_cast<const __m256i*>(str);
+  const __m256i *const mend = reinterpret_cast<const __m256i*>(end - 32);
   if (likely(len <= 255)) {
     // A lot of these values are read from possibly unaligned memory.
     // This is ok on all platforms supporting SSE2. (todo confirm)
@@ -407,78 +409,78 @@ bool unescaped_ascii_avx2(const char *str, const char *end) {
     case 234: case 233: case 232: case 231: case 230: case 229: case 228:
     case 227: case 226: case 225:
       return unescaped_ascii_avx2_helper_32_x8(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 192)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 160)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 128)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 96)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 64)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 6),
+        _mm256_loadu_si256(mstr + 5),
+        _mm256_loadu_si256(mstr + 4),
+        _mm256_loadu_si256(mstr + 3),
+        _mm256_loadu_si256(mstr + 2),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 224: case 223: case 222: case 221: case 220: case 219: case 218:
     case 217: case 216: case 215: case 214: case 213: case 212: case 211:
     case 210: case 209: case 208: case 207: case 206: case 205: case 204:
     case 203: case 202: case 201: case 200: case 199: case 198: case 197:
     case 196: case 195: case 194: case 193:
       return unescaped_ascii_avx2_helper_32_x7(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 160)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 128)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 96)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 64)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 5),
+        _mm256_loadu_si256(mstr + 4),
+        _mm256_loadu_si256(mstr + 3),
+        _mm256_loadu_si256(mstr + 2),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 192: case 191: case 190: case 189: case 188: case 187: case 186:
     case 185: case 184: case 183: case 182: case 181: case 180: case 179:
     case 178: case 177: case 176: case 175: case 174: case 173: case 172:
     case 171: case 170: case 169: case 168: case 167: case 166: case 165:
     case 164: case 163: case 162: case 161:
       return unescaped_ascii_avx2_helper_32_x6(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 128)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 96)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 64)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 4),
+        _mm256_loadu_si256(mstr + 3),
+        _mm256_loadu_si256(mstr + 2),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 160: case 159: case 158: case 157: case 156: case 155: case 154:
     case 153: case 152: case 151: case 150: case 149: case 148: case 147:
     case 146: case 145: case 144: case 143: case 142: case 141: case 140:
     case 139: case 138: case 137: case 136: case 135: case 134: case 133:
     case 132: case 131: case 130: case 129:
       return unescaped_ascii_avx2_helper_32_x5(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 96)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 64)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 3),
+        _mm256_loadu_si256(mstr + 2),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 128: case 127: case 126: case 125: case 124: case 123: case 122:
     case 121: case 120: case 119: case 118: case 117: case 116: case 115:
     case 114: case 113: case 112: case 111: case 110: case 109: case 108:
     case 107: case 106: case 105: case 104: case 103: case 102: case 101:
     case 100: case 99: case 98: case 97:
       return unescaped_ascii_avx2_helper_32_x4(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 64)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 2),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 96: case 95: case 94: case 93: case 92: case 91: case 90: case 89:
     case 88: case 87: case 86: case 85: case 84: case 83: case 82: case 81:
     case 80: case 79: case 78: case 77: case 76: case 75: case 74: case 73:
     case 72: case 71: case 70: case 69: case 68: case 67: case 66: case 65:
       return unescaped_ascii_avx2_helper_32_x3(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str + 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr + 1),
+        _mm256_loadu_si256(mstr)) == 0;
     case 64: case 63: case 62: case 61: case 60: case 59: case 58: case 57:
     case 56: case 55: case 54: case 53: case 52: case 51: case 50: case 49:
     case 48: case 47: case 46: case 45: case 44: case 43: case 42: case 41:
     case 40: case 39: case 38: case 37: case 36: case 35: case 34: case 33:
       return unescaped_ascii_avx2_helper_32_x2(
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(end - 32)),
-        _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+        _mm256_loadu_si256(mend),
+        _mm256_loadu_si256(mstr)) == 0;
     case 32:
       return unescaped_ascii_avx2_helper_32(
-              _mm256_loadu_si256(reinterpret_cast<const __m256i*>(str))) == 0;
+              _mm256_loadu_si256(mstr)) == 0;
     case 31: case 30: case 29: case 28: case 27: case 26: case 25: case 24:
     case 23: case 22: case 21: case 20: case 19: case 18: case 17:
       return unescaped_ascii_avx2_helper_32(
@@ -546,7 +548,7 @@ bool unescaped_ascii_avx2(const char *str, const char *end) {
   {
     if (unlikely(unescaped_ascii_avx2_helper_32(
                    _mm256_loadu_si256(
-                     reinterpret_cast<const __m256i*>(str))))) {
+                     mstr)))) {
       return false;
     }
   }
@@ -564,7 +566,7 @@ bool unescaped_ascii_avx2(const char *str, const char *end) {
   {
     if (unlikely(unescaped_ascii_avx2_helper_32(
                    _mm256_loadu_si256(
-                     reinterpret_cast<const __m256i*>(end - 32))))) {
+                     mend)))) {
       return false;
     }
   }
